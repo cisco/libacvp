bcc440c43f0d3c7259e3844ff2376eb0badcaa48
jfigus <foleyj@cisco.com>
2016-07-29 14:54:29 -0400

    Start to lay out directory structure.  Update license and provide overview information in README.

---
+++ scripts/nist_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export ACV_URI_PREFIX=acvp/
+export ACV_CA_FILE=certs/acvp.nist.gov.crt
+export ACV_KEY_FILE=certs/client_key.pem
+export ACV_CERT_FILE=certs/client_cert.pem
+export ACV_PORT=443
+export ACV_SERVER=acvp.nist.gov
+
