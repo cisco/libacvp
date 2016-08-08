06be962d297f6285508202c007c2196e4a141aa4
jfigus <foleyj@cisco.com>
2016-08-08 14:44:23 -0400

    Add certs and setup script for Cisco ACVP test server.

---
+++ scripts/cisco_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export ACV_URI_PREFIX=
+export ACV_CA_FILE=certs/cisco_cert.pem
+export ACV_KEY_FILE=certs/client/cisco_key.pem
+export ACV_CERT_FILE=certs/client/cisco_cert.pem
+export ACV_PORT=17743
+export ACV_SERVER=128.107.5.81
+
