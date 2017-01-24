3fcb034a0fa69e6d320e79a6cc4ae1f5d3052b57
John Foley <jfoley.tarheel@gmail.com>
2017-01-24 15:21:19 -0500

    Resolve clang compiler warnings.

--- src/acvp_hash.c
+++ src/acvp_hash.c
@@ -59,7 +59,7 @@ ACVP_RESULT acvp_hash_kat_handler(ACVP_CTX *ctx, JSON_Object *obj)
      * Get the crypto module handler for this hash algorithm
      */
     alg_id = acvp_lookup_cipher_index(alg_str);
-    if (alg_id < 0) {
+    if (alg_id < ACVP_CIPHER_START) {
         acvp_log_msg(ctx, "ERROR: unsupported algorithm (%s)", alg_str);
         return (ACVP_UNSUPPORTED_OP);
     }
