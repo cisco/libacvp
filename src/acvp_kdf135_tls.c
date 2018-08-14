38103efb403dd39769dc5c004f110d39f19c2a0e
Philip Perricone <prperri@gmail.com>
2018-08-14 15:14:51 -0400

    Little fix for kdf135_tls macro

--- src/acvp_kdf135_tls.c
+++ src/acvp_kdf135_tls.c
@@ -221,7 +221,7 @@ ACVP_RESULT acvp_kdf135_tls_kat_handler (ACVP_CTX *ctx, JSON_Object *obj) {
             }
             if (strnlen(pm_secret, pm_len) != pm_len/8) {
                 ACVP_LOG_ERR("pmLen(%d) or pmSecret length(%d) incorrect",
-                              pm_len/8, strnlen(pm_secret, ACVP_KDF135_TLS_PMSECRET_HEX_MAX));
+                              pm_len/8, strnlen(pm_secret, ACVP_KDF135_TLS_PMSECRET_STR_MAX));
                 return ACVP_INVALID_ARG;
             }
 
