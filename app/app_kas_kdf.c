c5e86171875f4dac586690fd5a3c1ba69891fe11
Andrew Karcher <abkarcher@gmail.com>
2021-01-12 08:36:10 -0500

    Missing kas kdf files added

---
+++ app/app_kas_kdf.c
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2020, Cisco Systems, Inc.
+ *
+ * Licensed under the Apache License 2.0 (the "License").  You may not use
+ * this file except in compliance with the License.  You can obtain a copy
+ * in the file LICENSE in the source distribution or at
+ * https://github.com/cisco/libacvp/LICENSE
+ */
+
+#include "acvp/acvp.h"
+#include "app_lcl.h"
+
+int app_kas_hkdf_handler(ACVP_TEST_CASE *test_case) {
+    if (!test_case) {
+        return -1;
+    }
+    return 0;
+}
+
+int app_kas_kdf_onestep_handler(ACVP_TEST_CASE *test_case) {
+    if (!test_case) {
+        return -1;
+    }
+    return 0;
+}
