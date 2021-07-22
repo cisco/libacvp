312c2f6dd592de2b28118918363bd37dfb6a6853
livebe01 <benjamin.livelsberger@nist.gov>
2021-07-22 15:59:54 -0400

    Create nist_setup.zsh
    -MacOS uses zsh by default, so having a nist_setup.zsh file is handy.
    -Alternatively, could tell users in the README to do this by hand.

---
+++ scripts/nist_setup.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+export ACV_URI_PREFIX=/acvp/v1/
+export ACV_API_CONTEXT=acvp/
+export ACV_CA_FILE=certs/acvp.nist.gov.crt
+export ACV_KEY_FILE=<user generated>
+export ACV_CERT_FILE=<procure from nist>
+export ACV_TOTP_SEED=<procure from nist>
+export ACV_PORT=443
+export ACV_SERVER=demo.acvts.nist.gov
+
