1747deff7d5a1f30eedccb2463a2f474f2d2d46d
Andrew Karcher <9059272+abkarcher@users.noreply.github.com>
2023-05-15 08:13:57 -0400

    Remove python, fix typo

--- test/json/ecdsa/__init__.py
+++
@@ -1,4 +0,0 @@
-from .ecdsa import main_ecdsa_kg
-from .ecdsa import main_ecdsa_kv
-from .ecdsa import main_ecdsa_sg
-from .ecdsa import main_ecdsa_sv
--- test/json/rsa/__init__.py
+++
@@ -1,3 +0,0 @@
-from .rsa_keygen import main_rsa_keygen
-from .rsa_siggen import main_rsa_siggen
-from .rsa_sigver import main_rsa_sigver
