b7e8d9584c52d28056172ec7d3f5757f6ff1e3e5
Andrew Karcher <9059272+abkarcher@users.noreply.github.com>
2024-08-12 15:18:13 +0000

    Configure updates, safeC for TOTP code

--- app/totp/hmac_sha256.c
+++ app/totp/hmac_sha256.c
@@ -1,6 +1,10 @@
 /*
    hmac_sha256.c
    Originally written by https://github.com/h5p9sl
+   LIBACVP NOTE: Original code modified to include PR #7 at https://github.com/h5p9sl/hmac_sha256/pull/7.
+   Code also modified to use safeC memory calls.
+   This code is not thoroughly vetted and should NOT be used for any security purposes. It should ONLY be used for
+   TOTP generation for the already-secured ACVP protocol communications.
  */
 
 #include "hmac_sha256.h"
@@ -8,6 +12,7 @@
 
 #include <stdlib.h>
 #include <string.h>
+#include "safe_lib.h"
 
 #define SHA256_BLOCK_SIZE 64
 
@@ -42,16 +47,16 @@ size_t hmac_sha256(const void* key,
   size_t sz;
   int i;
 
-  memset(k, 0, sizeof(k));
-  memset(k_ipad, 0x36, SHA256_BLOCK_SIZE);
-  memset(k_opad, 0x5c, SHA256_BLOCK_SIZE);
+  memset_s (k, sizeof(k), 0, sizeof(k));
+  memset_s (k_ipad, sizeof(k_ipad), 0x36, sizeof(k_ipad));
+  memset_s(k_opad, sizeof(k_opad), 0x5c, sizeof(k_opad));
 
   if (keylen > SHA256_BLOCK_SIZE) {
     // If the key is larger than the hash algorithm's
     // block size, we must digest it first.
     sha256(key, keylen, k, sizeof(k));
   } else {
-    memcpy(k, key, keylen);
+    memcpy_s(k, sizeof(k), key, keylen);
   }
 
   for (i = 0; i < SHA256_BLOCK_SIZE; i++) {
@@ -65,7 +70,7 @@ size_t hmac_sha256(const void* key,
   H(k_opad, sizeof(k_opad), ihash, sizeof(ihash), ohash, sizeof(ohash));
 
   sz = (outlen > SHA256_HASH_SIZE) ? SHA256_HASH_SIZE : outlen;
-  memcpy(out, ohash, sz);
+  memcpy_s(out, outlen, ohash, sz);
   return sz;
 }
 
@@ -75,16 +80,23 @@ static void* H(const void* x,
                const size_t ylen,
                void* out,
                const size_t outlen) {
-  void* result;
-  size_t buflen = (xlen + ylen);
-  uint8_t* buf = (uint8_t*)malloc(buflen);
 
-  memcpy(buf, x, xlen);
-  memcpy(buf + xlen, y, ylen);
-  result = sha256(buf, buflen, out, outlen);
+  size_t sz;
+  Sha256Context ctx;
+  SHA256_HASH hash;
 
-  free(buf);
-  return result;
+  Sha256Initialise(&ctx);
+  Sha256Update(&ctx, x, xlen);
+  Sha256Update(&ctx, y, ylen);
+  Sha256Finalise(&ctx, &hash);
+
+  sz = (outlen > SHA256_HASH_SIZE) ? SHA256_HASH_SIZE : outlen;
+
+  if (memcpy_s(out, outlen, hash.bytes, sz) == EOK) {
+    return out;
+  } else {
+    return NULL;
+  }
 }
 
 static void* sha256(const void* data,
@@ -100,5 +112,9 @@ static void* sha256(const void* data,
   Sha256Finalise(&ctx, &hash);
 
   sz = (outlen > SHA256_HASH_SIZE) ? SHA256_HASH_SIZE : outlen;
-  return memcpy(out, hash.bytes, sz);
+  if (memcpy_s(out, outlen, hash.bytes, sz) == EOK) {
+    return out;
+  } else {
+    return NULL;
+  }
 }
