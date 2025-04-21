15274f864468c594c5ce558e1ca696d242a2f584
Andrew Karcher <9059272+abkarcher@users.noreply.github.com>
2025-04-21 13:02:21 +0000

    Fix typo

--- include/acvp/acvp.h
+++ include/acvp/acvp.h
@@ -11,7 +11,7 @@
  * this file except in compliance with the License. You can obtain a copy
  * in the file LICENSE in the source distribution or at
  * https://github.com/cisco/libacvp/LICENSE
-, */
+ */
 
 #ifndef acvp_h
 #define acvp_h
