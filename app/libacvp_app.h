f94e23db219992265a15b6672884affe47a61349
Andrew Karcher <9059272+abkarcher@users.noreply.github.com>
2025-07-24 13:30:00 -0400

    Copyright year updates and one last readme update

--- app/libacvp_app.h
+++ app/libacvp_app.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
-* Copyright (c) 2021, Cisco Systems, Inc.
+* Copyright (c) 2025, Cisco Systems, Inc.
 * All rights reserved.
 
 * Redistribution and use in source and binary forms, with or without modification,
